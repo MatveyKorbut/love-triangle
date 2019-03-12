4cc9d793c458fbf76a19e6429899d59f50cb82fd
Matvey Korbut <korbutmatvey@gmail.com>
2019-03-12 11:07:51 +0300

    feat: add js code

--- src/index.js
+++ src/index.js
@@ -3,5 +3,19 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count=0;  
+  var triangles=0;
+  for(var i=0;i<preferences.length;i++){ 
+    var first=preferences[i];
+    var second=preferences[first-1];
+    var third=preferences[second-1];
+    if(third==i+1){
+      triangles++;
+    }
+    if (triangles==3){
+      count++;
+      triangles=0;
+    }
+  }
+   return(count);
 };
